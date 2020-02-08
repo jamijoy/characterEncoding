e4ec3bc9e3d9645fc0d4ced98613af3417b86dc2
Jami <jamijoyy@gmail.com>
2020-02-08 15:11:06 +0600

    Decoder Functionality Added.

--- CharEncoding.php
+++ CharEncoding.php
@@ -1,6 +1,6 @@
 <?php
 // print_r(explode("","How you are today?"));
-// print_r(str_split("Simple Typing Text Quality"));
+// print_r(str_split("American International University Bangladesh"));
 
 function splitter($objSplitter)
 {
@@ -19,10 +19,60 @@ function splitter($objSplitter)
 			$strResult = strval($strResult) . strval($str[$i]);
 		}
 	}
-	print_r("<br/>".$strResult);
-	//decoder($strResult);
+	//print_r("<br/>".$strResult);
+	decoder($strResult);
 }
 
+function decoder($objSplitter)
+{
+	echo " : ".$objSplitter." : ";
+	$msgArray = explode(" ",$objSplitter);
+	for($i=0; $i<count($msgArray); $i++)
+	{
+		$chnk = $msgArray[$i];
+		//echo (empty($chnk)) ? "Empty" : "Not empty";
+		while(trim($chnk)!="")
+		{
+			$chnky = str_split($chnk,2);
+			// print_r($chnky);
+			// echo "[".$chnky[0].":"."]";
+			
+			if(($chnky[0]>64 && $chnky[0]<91) || ($chnky[0]>96 && $chnky[0]<123))
+			{
+				echo chr($chnky[0]);
+				if(isset($chnky[1]))
+				{
+					$chnk = substr($chnk,2);
+					// echo "||".$chnk."||";
+				}
+				else
+				{
+					$chnk = "";
+				}
+			}
+			else
+			{
+				$chnky = str_split($chnk,3);
+				echo chr($chnky[0]);
+				if(isset($chnky[1]))
+				{
+					$chnk = substr($chnk,3);
+					// echo "||".$chnk."||";
+				}
+				else
+				{
+					$chnk = "";
+				}
+			}
+			// break;
+		}
+		
+			
+		echo " ";
+		// print_r(str_split($msgArray[$i],2));
+	}
+	//print_r($msgArray);
+}
 
-splitter("Type here texts to perform cases!");
+splitter("here you go titans");
 ?>
